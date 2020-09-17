741c8a4ccbd458f02d075432595c9f33d312ec63
Sabbir Islam Mukdo <bmukdo@gmail.com>
2020-09-18 02:53:21 +0600

    complete view post

--- SimpleBlog/Blog.Web/Controllers/HomeController.cs
+++ SimpleBlog/Blog.Web/Controllers/HomeController.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Blog.Web.Models;
+using Blog.Framework;
 
 namespace Blog.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private FrameworkContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , FrameworkContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
 
         public IActionResult Index()
         {
+            var post = _context.BlogComposes.OrderByDescending(d => d.Date);
+            ViewBag.Post = post;
             return View();
         }
 
